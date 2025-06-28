ffe96937f4b687f505dcb06947d33b305468b2c9
sheikh muhammad samir hoda <sheikh_081bct036@tcioe.edu.np>
2025-06-28 13:34:26 +0545

    removed member fun input

--- skp.cpp
+++ skp.cpp
@@ -1,36 +1,31 @@
 #include<iostream>
 using namespace std;
-class complex{
+
+class complex1{
     private:
      int real,img;
     public:
-    void input(){
-        cout<<"Enter the real and imaginary part :"<<endl;
-        cin>>real>>img;
-
-    }
-    complex(){
+    complex1(){
         real = 0;
         img = 0;
     }
-    complex(int r, int i){
+    complex1(int r, int i){
         real = r;
         img = i;
     }
-    void addcom(complex c1,complex c2){
-        real = c1.real + c2.real;
-        img = c1.img + c2.img;
+    void addcom(complex1 c2){
+        real = real + c2.real;
+        img = img + c2.img;
     }
     void display(){
         cout<<"The sum of the two complex numbers is: "<<real<<" + "<<img<<"i"<<endl;
     }
 
 };
+
 int main(){
-    complex c1, c2, c3;
-    c1.input();
-    c2.input();
-    c3.addcom(c1, c2);
-    c3.display();
+    complex1 c1(2,4), c2(3,5);
+    c1.addcom(c2);
+    c1.display();
     return 0;
 }
