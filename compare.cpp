68ba3742e6c37c2eb43b2aef50239358c1f6f8f9
Shivam Kumar Sah <shivam_081bct039@tcioe.edu.np>
2025-06-28 13:18:02 +0545

    Adding a compare file in my projext

---
+++ compare.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class compare{
+    private:
+    int a;
+    public:
+    void input(){
+        cout<<"enter the value of a: "<<endl;
+        cin>>a;
+    }
+    int operator <= (compare c){
+        return(a<=c.a?1:0);
+    }
+};
+int main(){
+    compare c1,c2;
+    c1.input();
+    c2.input();
+    if(c1<=c2){
+        cout<<"c1 is less than or equal to c2"<<endl;
+    } else {
+        cout<<"c1 is greater than c2"<<endl;
+    }
+}
